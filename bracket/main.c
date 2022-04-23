a0e241b5f6080095819605958adc909f79fb1e81
Yujeong Kim <yukim@c1r4s1.42seoul.kr>
2022-04-23 16:23:16 +0900

    .

--- bracket/main.c
+++ bracket/main.c
@@ -1,12 +1,11 @@
-#include <stdio.h>
 #include "linkedstack.h"
 
 int main()
 {
 
-	// char		*str = "AB(CDE)}";
+	char		*str = "AB(CDE)";
 
-	// printf("BOOL : %d\n", checkBracket(str));
+	printf("BOOL : %d\n", checkBracket(str));
 
 	printf("!\n");
 	
